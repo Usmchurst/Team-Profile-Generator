a943995709b0f10af6d52ca56d9a1b983583743d
Amanda Rodriguez <amandardz15@gmail.com>
2021-04-17 22:39:25 -0500

    modifying files

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -2,25 +2,29 @@ const Manager = require('../lib/Manager');
 const Engineer = require('../lib/Engineer')
 const Intern = require('../lib/Intern')
 
+
 function generateCards(team) {
+  let cards = []
   for(let i = 0; i < team.length; i++) {
     const teamArray = team[i];
     switch(teamArray.getRole()) {
       case 'Manager':
         const manager = new Manager(teamArray.id, teamArray.name, teamArray.email, teamArray.officeNumber);
-        return generateManagerCard(manager);
+        cards.push(generateManagerCard(manager));
         break;
       case 'Engineer':
         const engineer = new Engineer(teamArray.id, teamArray.name, teamArray.email, teamArray.github);
-        return generateEngineerCard(engineer);
+        cards.push(generateEngineerCard(engineer));
         break;
       case 'Intern':
         const intern = new Intern(teamArray.id, teamArray.name, teamArray.email, teamArray.school);
-        return generateInternCard(intern);
+        cards.push(generateInternCard(intern));
         break;
     }
   }
+  return cards.join(``)
 }
+
 let generateManagerCard = (Manager) => {
   return `
   <h5 class="card-title">${Manager.getName()}</h5>
